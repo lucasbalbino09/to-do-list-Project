95a97d2f564b428d3b733a7720312a1fec50e421
lucas.balbino <98754988+lucasbalbino09@users.noreply.github.com>
2023-09-06 10:22:10 -0300

    colocando o projeto no GitPages

--- src/app/module/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/module/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoInputAddItensComponent implements OnInit {
 
+  @Output() EmitItemListaCompleta = new EventEmitter();
+
+  adiconarItensnaLista : string = "";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  enviarItensaLista() : void {
+    
+    this.adiconarItensnaLista = this.adiconarItensnaLista.trim()
+    
+    if(this.adiconarItensnaLista){
+      this.EmitItemListaCompleta.emit(this.adiconarItensnaLista);
+      this.adiconarItensnaLista = "";
+    }
   }
 
 }
--- src/app/module/home/components/todo-list/todo-list.component.ts
+++ src/app/module/home/components/todo-list/todo-list.component.ts
@@ -1,20 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 // interface
-import {TaskList} from '../../model/task-list';
+import { TaskList } from '../../model/task-list';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.less']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
 
-  listaCompleta : Array<TaskList> = [
-    {task: 'Minha nova Task', checked: true},
-    {task: 'Minha nova Task 2', checked: false}
-  ];
+  listaCompleta: Array<TaskList> = JSON.parse(localStorage.getItem("lista") || '[]');
   constructor() { }
 
-  ngOnInit(): void {
-    
+  ngDoCheck(): void {
+    this.setLocalStorage();
+  }
+
+  setEmitLista(event: string) {
+    this.listaCompleta.push({ task: event, checked: false });
+  }
+
+  deletarItemDaLista(event: number): void {
+    this.listaCompleta.splice(event, 1);
+  }
+
+  deletaTudo(): void {
+    const confirm = window.confirm("Você Realmente deseja Excluir tudo ?");
+    if (confirm == true) {
+      this.listaCompleta = [];
+    }
+  }
+
+  validacaoDoInput(event: string, index: number) {
+    if(!event.length){
+      const confirm = window.confirm("O item está vazio, deseja deletar ? ")
+
+      if(confirm){
+        this.deletarItemDaLista(index);
+      } 
+    }
+  }
+
+  setLocalStorage(){
+    if(this.listaCompleta) {
+      this.listaCompleta.sort((first, last) => Number(first.checked) - Number(last.checked))
+      localStorage.setItem("lista",JSON.stringify(this.listaCompleta))
+    }
   }
 }
